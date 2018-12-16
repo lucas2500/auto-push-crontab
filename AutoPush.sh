8ffdaf0bc32def45d802e69aa259904c1335e694
Lucas Rafael <lucasrafael654@gmail.com>
2018-12-16 02:38:01 +0000

    <Atualizando>

--- AutoPush.sh
+++ AutoPush.sh
@@ -3,10 +3,6 @@ git -C /var/backups/teste/ add --all
 
 git -C /var/backups/teste/ commit -m '<Atualizando>'
 
-git -C /var/backups/teste/ config user.nome "Lucas Rafael"
-
-git -C /var/backups/teste/ config user.email "lucasrafael654@gmail.com"
-
 git -C /var/backups/teste/ config remote.origin.url https://lucas2500:bombapatch36@github.com/lucas2500/auto-push-crontab.git
 
 git -C /var/backups/teste/ push -u origin master 
