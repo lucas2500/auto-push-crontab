7160704709c1148f31abbf6eb244d47c37debd16
Lucas Rafael <lucasrafael654@gmail.com>
2018-12-16 01:45:01 +0000

    <updating>

--- AutoPush.sh
+++ AutoPush.sh
@@ -5,6 +5,6 @@ git -C /var/backups/teste/ commit -m '<updating>'
 
 git -C /var/backups/teste/ config user.name "Lucas Rafael"
 
-git -C /var/backups/teste/ config user.email "lucasrafael654@gmail.com"
+git -C /var/backups/teste/ config credential.https://github.com.username <lucas2500>
 
 git -C /var/backups/teste/ push origin master 
