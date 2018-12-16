294ed4f72d6129b4ef698a60bcf8b13d42dc5ab8
root <lucasrafael654@gmail.com>
2018-12-16 00:14:01 +0000

    <updating>

--- AutoPush.sh
+++ AutoPush.sh
@@ -1,6 +1,8 @@
 
 git -C /var/backups/teste/ add --all
 
+git -C /var/backups/teste/ config user.email "lucasrafael654@gmail.com"
+
 git -C /var/backups/teste/ commit -m '<updating>'
 
 git -C /var/backups/teste/ push origin master 
