b84e2a05d33f68cf2ebbed563c966c14f60f5b97
lucasrafael654@gmail.com <lucasrafael654@gmail.com>
2018-12-16 01:17:01 +0000

    <updating>

--- AutoPush.sh
+++ AutoPush.sh
@@ -3,4 +3,6 @@ git -C /var/backups/teste/ add --all
 
 git -C /var/backups/teste/ commit -m '<updating>'
 
+git -C /var/backups/teste/ git config --global user.email "lucasrafael654@gmail.com"
+
 git -C /var/backups/teste/ push origin master 
