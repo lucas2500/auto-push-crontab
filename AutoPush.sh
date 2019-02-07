b1f74b7727b5d5dd2d48a75ebd38686ba97f58f3
unknown <lucasrafael654@gmail.com>
2019-02-07 14:32:33 -0300

    <Excluindo arquivos de backup do banco>

--- AutoPush.sh
+++ AutoPush.sh
@@ -4,6 +4,6 @@ git -C /var/backups/teste/ add --all
 
 git -C /var/backups/teste/ commit -m '<Atualizando>'
 
-git -C /var/backups/teste/ config remote.origin.url https://lucas2500:bombapatch36@github.com/lucas2500/auto-push-crontab.git
+git -C /var/backups/teste/ config remote.origin.url https://username:password@github.com/lucas2500/auto-push-crontab.git
 
 git -C /var/backups/teste/ push -u origin master 
